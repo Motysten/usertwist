aefd642526bd5b38ae23428f215ee680a4238ada
Wilfried OLLIVIER <wollivier@fdn.fr>
2024-07-18 00:47:09 +0200

    chore: harmonize handlers pattern

--- internal/http/handlers/banner.go
+++ internal/http/handlers/banner.go
@@ -5,8 +5,6 @@ import (
 	"net/http"
 )
 
-func Banner() http.HandlerFunc {
-	return func(writer http.ResponseWriter, request *http.Request) {
-		_, _ = fmt.Fprintf(writer, "usertwist\n")
-	}
+func Banner(writer http.ResponseWriter, request *http.Request) {
+	_, _ = fmt.Fprintf(writer, "usertwist\n")
 }
